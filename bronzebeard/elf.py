5f6ea80a920980cb28dccf7bc0803c8593e8c21b
theandrew168 <steveday168@gmail.com>
2020-08-26 15:57:32 -0500

    Clarify some header fields

--- bronzebeard/elf.py
+++ bronzebeard/elf.py
@@ -18,15 +18,15 @@ class ELF:
         elf.extend(b'\x02\x00')  # type: executable
         elf.extend(b'\x3e\x00')  # machine: amd64
         elf.extend(b'\x01\x00\x00\x00')  # version: 1
-        elf.extend(b'\x00\x00\x40\x00\x00\x00\x00\x00')  # TODO entry point: 0x400000 + offset to _start?
-        elf.extend(b'\x40\x00\x00\x00\x00\x00\x00\x00')  # TODO program header table offset: 0x40 usually?
-        elf.extend(b'\x80\x00\x00\x00\x00\x00\x00\x00')  # TODO section header table offset: 0x80 not sure?
+        elf.extend(b'\x00\x00\x40\x00\x00\x00\x00\x00')  # TODO entry point: 0x400000 + offset to _start (0x401000)
+        elf.extend(b'\x40\x00\x00\x00\x00\x00\x00\x00')  # program header table offset: 0x40
+        elf.extend(b'\x80\x00\x00\x00\x00\x00\x00\x00')  # TODO section header table offset: depends
         elf.extend(b'\x00\x00\x00\x00')  # flags: 0
         elf.extend(b'\x40\x00')  # header size: 64 bytes
-        elf.extend(b'\x40\x00')  # TODO program header entry size: 64 bytes?
-        elf.extend(b'\x01\x00')  # TODO program header entries: depends?
-        elf.extend(b'\x40\x00')  # TODO section header entry size: 64 bytes?
-        elf.extend(b'\x04\x00')  # TODO section header entries: depends?
-        elf.extend(b'\x03\x00')  # TODO: index of names in section header entry: 3?
+        elf.extend(b'\x40\x00')  # program header entry size: 64 bytes
+        elf.extend(b'\x01\x00')  # TODO program header entries: depends
+        elf.extend(b'\x40\x00')  # section header entry size: 64 bytes
+        elf.extend(b'\x04\x00')  # TODO section header entries: depends
+        elf.extend(b'\x04\x00')  # index of names in section header entry: 4
 
         return bytes(elf)
