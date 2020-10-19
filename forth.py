589c1cbaca225740b164b4a0bf1f25c2e15a0fb6
theandrew168 <steveday168@gmail.com>
2020-10-19 15:31:31 -0500

    Add more details to forth.py

--- forth.py
+++ forth.py
@@ -4,6 +4,16 @@ from simpleriscv import asm
 # https://github.com/cesarblum/sectorforth
 # https://www.bradrodriguez.com/papers/moving1.htm
 
+# GD32VF103[CBT6]: Longan Nano, Wio Lite
+ROM_BASE_ADDR = 0x08000000  # 128K
+RAM_BASE_ADDR = 0x20000000  # 32K
+
+# FE310-G002: HiFive1 Rev B
+#ROM_BASE_ADDR  = 0x20000000  # 4M
+#ROM_BASE_ADDR += 0x00010000  # first 64K of ROM is taken by the bootloader
+#RAM_BASE_ADDR  = 0x80000000  # 16K
+
+
 #       Register Assignment
 # |------------------------------|
 # | reg | name |   description   |
@@ -11,7 +21,7 @@ from simpleriscv import asm
 # |  x0 | zero | always 0        |
 # |  x1 |  ra  | working reg     |
 # |  x2 |  sp  | data stack ptr  |
-# |  x3 |  gp  | forth inst ptr  |
+# |  x3 |  gp  | interpreter ptr |
 # |  x4 |  tp  | ret stack ptr   |
 # |  x5 |  t0  | temp reg 0      |
 # |  x6 |  t1  | temp reg 1      |
@@ -21,7 +31,18 @@ from simpleriscv import asm
 # | x18 |  s2  | >IN var         |
 # | x19 |  s3  | HERE var        |
 # | x20 |  s4  | LATEST var      |
+# | x28 |  t3  | temp reg 3      |
+# | x29 |  t4  | temp reg 4      |
+# | x30 |  t5  | temp reg 5      |
+# | x31 |  t6  | temp reg 6      |
 # |------------------------------|
+
+# "The Classical Forth Registers"
+R_W = 'ra'
+R_IP = 'gp'
+R_DSP = 'sp'
+R_RSP = 'tp'
+
 
 #                Variables
 # |----------------------------------------|
@@ -34,47 +55,64 @@ from simpleriscv import asm
 # | LATEST | ptr to most recent dict entry |
 # |----------------------------------------|
 
-#            Memory Map
-#        |----------------|
+# Variable registers
+R_STATE = 's0'
+R_TIB = 's1'
+R_TOIN = 's2'
+R_HERE = 's3'
+R_LATEST = 's4'
+
+
+#  16KB      Memory Map
+# 0x0000 |----------------|
 #        |                |
-#        |  Return Stack  |
-#        |                |
-# 0x3000 |----------------|
-#        |                |
-#        |   Data Stack   |
+#        |  Interpreter   |
+#        |       +        |
+# 0x1000 |      TIB       |
+#        |       +        |
+#        |   Dictionary   |
 #        |                |
 # 0x2000 |----------------|
 #        |                |
-#        |   Dictionary   |
+#        |   Data Stack   |
 #        |                |
-# 0x1000 |----------------|
+# 0x3000 |----------------|
 #        |                |
-#        |   Interpreter  |
+#        |  Return Stack  |
 #        |                |
-# 0x0000 |----------------|
+# 0x3FFF |----------------|
 
-# Total RAM: 16KB
-# Each section gets 4KB
-RETURN_STACK_BASE = 0x3000
-DATA_STACK_BASE = 0x2000
-DICTIONARY_BASE = 0x1000
 INTERPRETER_BASE = 0x0000
+DATA_STACK_BASE = 0x2000
+RETURN_STACK_BASE = 0x3000
 
 
-PREV_WORD = None
+F_IMMEDIATE = 0x80
+F_HIDDEN = 0x40
+LEN_MASK = 0x1f
+
+LINK = None
 def defword(p, name, label, flags=0):
+    """
+    Macro to define a Forth word. The LINK stuff is really hacky and
+    I'd like to replace it with something else. The "flags" param be any
+    or'd combo of the 2 F_FOO constants defined above.
+    """
+
     if len(name) > 0x1f:
-        raise ValueError('Word name is longer than 0x1f (31): {}'.format(name))
+        raise ValueError('Word name is longer than 0x1f (31 chars): {}'.format(name))
 
     word_label = 'word_' + label
     p.LABEL(word_label)
 
-    global PREV_WORD
-    if PREV_WORD is None:
+    # these links will above be relative and negative
+    #  (since new words are higher in the address space)
+    global LINK
+    if LINK is None:
         link = 0
     else:
-        link = p.labels[PREV_WORD] - p.labels[word_label]
-    PREV_WORD = word_label
+        link = p.labels[LINK] - p.labels[word_label]
+    LINK = word_label
 
     p.BLOB(struct.pack('<h', link))
     p.BLOB(struct.pack('<B', len(name)))
@@ -83,7 +121,10 @@ def defword(p, name, label, flags=0):
 
     p.LABEL(name)
 
+
 p = asm.Program()
+with p.LABEL('copy'):
+    p.LUI
 with p.LABEL('start'):
     p.JAL('zero', 'init')
 with p.LABEL('error'):
