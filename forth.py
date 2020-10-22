fab7121642cffc4cae392b969a23b7b6cb71088d
theandrew168 <steveday168@gmail.com>
2020-10-22 14:20:02 -0500

    More work on searching the word dict

--- forth.py
+++ forth.py
@@ -112,9 +112,9 @@ INTERPRETER_BASE = 0x0000
 DATA_STACK_BASE = 0x2000
 RETURN_STACK_BASE = 0x3000
 
-F_IMMEDIATE = 0x80
-F_HIDDEN = 0x40
-LEN_MASK = 0x1f
+F_IMMEDIATE = 0b10000000
+F_HIDDEN    = 0b01000000
+LEN_MASK    = 0b00011111
 
 LINK = None
 
@@ -136,6 +136,15 @@ def defword(p, name, label, flags=0):
 
     # these links will above be relative and negative
     #  (since new words are higher in the address space)
+    # TODO: this is just too hacky: doesn't respect defer, digs into labels...
+    # Feature: deferred blob that refs a label, specify the size? 8, 16, 32, etc
+    #   but then it also updates a var? almost seems cleaner to just collect
+    #   the "normal" segments (label, inst, blob, align) into a list and make
+    #   a first pass that just collects label locations. then the second pass
+    #   handle "macros" and does the actual assembling? Second pass can include
+    #   an API for querying label locations since they'll all be present.
+    #   Immediate values can also use this. Maybe have two functions, one for
+    #   raw location and one for relative offset? (for PIC on jump / branch insts)
     global LINK
     if LINK is None:
         link = 0
@@ -143,8 +152,8 @@ def defword(p, name, label, flags=0):
         link = p.labels[LINK] - p.labels[word_label]
     LINK = word_label
 
-    p.BLOB(struct.pack('<B', flags | len(name)))
     p.BLOB(struct.pack('<h', link))
+    p.BLOB(struct.pack('<B', flags | len(name)))
     p.BLOB(name.encode())
     p.ALIGN()
 
@@ -153,9 +162,6 @@ def defword(p, name, label, flags=0):
 
 
 p = asm.Program()
-
-# code in ROM starts here
-# code for copying ROM to RAM starts here
 
 # t0 = src, t1 = dest, t2 = count
 with p.LABEL('copy'):
@@ -167,7 +173,6 @@ with p.LABEL('copy'):
     p.ADDI('t1', 't1', p.LO(RAM_BASE_ADDR))
     # setup copy count (everything up to the "here" label)
     p.ADDI('t2', 'zero', 'here')
-
 with p.LABEL('copy_loop'):
     p.BEQ('t2', 'zero', 'copy_done')
     p.LW('t3', 't0', 0)  # [src] -> t3
@@ -176,17 +181,12 @@ with p.LABEL('copy_loop'):
     p.ADDI('t1', 't1', 4)  # dest += 4
     p.ADDI('t2', 't2', -4)  # count -= 4
     p.JAL('zero', 'copy_loop')
-
 with p.LABEL('copy_done'):
     # jump to RAM:start
     p.LUI('t0', p.HI(RAM_BASE_ADDR))
     p.ADDI('t0', 't0', p.LO(RAM_BASE_ADDR))
     p.ADDI('t0', 't0', 'start')
     p.JALR('zero', 't0', 0)
-
-
-# code in RAM starts here
-# main Forth interpreter starts here
 
 with p.LABEL('start'):
     p.JAL('zero', 'init')
@@ -205,6 +205,11 @@ with p.LABEL('init'):
     # set working register to zero
     p.ADDI(W, 'zero', 0)
 
+    # set interpreter pointer to "interpreter" location
+    p.LUI(IP, p.HI(RAM_BASE_ADDR))
+    p.ADDI(IP, IP, p.LO(RAM_BASE_ADDR))
+    p.ADDI(IP, IP, 'interpreter')
+
     # set STATE var to zero
     p.ADDI(STATE, 'zero', 0)
 
@@ -212,6 +217,7 @@ with p.LABEL('init'):
     p.LUI(TIB, p.HI(RAM_BASE_ADDR))
     p.ADDI(TIB, TIB, p.LO(RAM_BASE_ADDR))
     p.ADDI(TIB, TIB, 'tib')
+    # TODO: fill TIB with zeroes
 
     # set TOIN var to zero
     p.ADDI(TOIN, 'zero', 0)
@@ -228,11 +234,39 @@ with p.LABEL('init'):
 
 # main interpreter loop
 with p.LABEL('interpreter'):
-    # TODO: what is the default for IP?
-    p.JAL('ra', 'token')  # call token procedure
+    p.JAL('ra', 'token')  # call token procedure (a0 = addr, a1 = len)
     p.ADDI('t0', 'zero', 3)
-    p.BEQ('a1', 't0', 'code_led')  # turn on LED if current token len == 3
-    p.JAL('zero', 'interpreter')
+    p.BEQ('a1', 't0', 'code_led')
+with p.LABEL('search'):
+    p.ADDI('t0', LATEST, 0)  # copy addr of latest word into t0
+with p.LABEL('search_loop'):
+    p.LH('t1', 't0', 0)  # load link of current word into t1
+    p.LBU('t2', 't0', 2)  # load flags / len of current word into t2
+    p.ANDI('t2', 't2', LEN_MASK)  # TODO wipe out flags for now leaving word length
+    p.BEQ('a1', 't2', 'search_compare')  # continue the search if length matches
+    p.BEQ('t1', 'zero', 'error')  # if link is zero then the word isn't found: error and reset
+    p.ADD('t0', 't0', 't1')  # point t0 at the next word (add the link offset)
+    p.JAL('zero', 'search_loop')  # continue the search
+with p.LABEL('search_compare_next'):
+    p.ADDI('t2', 't2', -1)  # dec len by 1
+    p.BEQ('t2', 'zero', 'search_found')  # if all chars have been checked, its a match!
+    p.ADDI('t3', 't3', 1)  # inc TIB ptr
+    p.ADDI('t4', 't4', 1)  # inc word dict ptr
+    p.JAL('zero', 'search_compare_loop')  # check next char
+with p.LABEL('search_compare'):
+    p.ADDI('t3', 'a0', 0)  # t3 points at name in TIB string
+    p.ADDI('t4', 't0', 3)  # t4 points at name in word dict
+with p.LABEL('search_compare_loop'):
+    p.LBU('t5', 't3', 0)  # load TIB char into t5
+    p.LBU('t6', 't4', 0)  # load dict char into t6
+    p.BEQ('t5', 't6', 'search_compare_next')  # continue comparing if current chars match
+    p.BEQ('t1', 'zero', 'error')  # if link is zero then the word isn't found: error and reset
+    p.ADD('t0', 't0', 't1')  # point t0 at the next word (add the link offset)
+    p.JAL('zero', 'search_loop')  # continue the search
+with p.LABEL('search_found'):
+    # word is found and located at t0
+    p.ADDI(W, 't0', 8)  # TODO: hack to manually skip name pad bytes
+    p.JAL('zero', 'next')  # execute the word!
 
 # TODO: handle running off the TIB (max 1024 bytes or something)
 with p.LABEL('token'):
@@ -278,7 +312,7 @@ with p.LABEL('exit'):
 
 with p.LABEL('tib'):
     # call the building "led" word
-    p.BLOB(b'     led ')
+    p.BLOB(b'led ')
 
     # make some numbers
     p.BLOB(b': dup sp@ @ ; ')
@@ -322,14 +356,14 @@ with p.LABEL('tib'):
 # dictionary starts here
 
 # TODO: implement colon
-with defword(p, ':', 'COLON'):
-    pass
+#with defword(p, ':', 'COLON'):
+#    pass
 
 # TODO: implement semicolon
-with defword(p, ';', 'SEMICOLON', flags=F_IMMEDIATE):
-    pass
+#with defword(p, ';', 'SEMICOLON', flags=F_IMMEDIATE):
+#    pass
 
-# this just exists to ensure that code got correctly copied from ROM to RAM
+p.LABEL('latest')  # mark the latest builtin word
 with defword(p, 'led', 'LED'):
     RCU_BASE_ADDR = 0x40021000
     RCU_APB2_ENABLE_OFFSET = 0x18
@@ -368,132 +402,132 @@ with defword(p, 'led', 'LED'):
     # next
     p.JAL('zero', 'next')
 
-with defword(p, 'state', 'STATEVAR'):
-    # push STATE onto stack
-    p.SW(DSP, STATE, 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, 'tib', 'TIBVAR'):
-    # push TIB onto stack
-    p.SW(DSP, TIB, 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, '>in', 'TOINVAR'):
-    # push TOIN onto stack
-    p.SW(DSP, TOIN, 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, 'here', 'HEREVAR'):
-    # push HERE onto stack
-    p.SW(DSP, HERE, 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, 'latest', 'LATESTVAR'):
-    # push LATEST onto stack
-    p.SW(DSP, LATEST, 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, '@', 'FETCH'):
-    # pop address into t0
-    p.ADDI(DSP, DSP, -4)
-    p.LW('t0', DSP, 0)
-    # load value from address
-    p.LW('t0', 't0', 0)
-    # push value onto stack
-    p.SW(DSP, 't0', 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, '!', 'STORE'):
-    # pop address into t0
-    p.ADDI(DSP, DSP, -4)
-    p.LW('t0', DSP, 0)
-    # pop value into t1
-    p.ADDI(DSP, DSP, -4)
-    p.LW('t1', DSP, 0)
-    # store value to address
-    p.SW('t0', 't1', 0)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, 'sp@', 'SPFETCH'):
-    # copy DSP into t0 and decrement to current top value
-    p.ADDI('t0', DSP, 0)
-    p.ADDI('t0', 't0', -4)
-    # push t0 onto stack
-    p.SW(DSP, 't0', 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, 'rp@', 'RPFETCH'):
-    # copy RSP into t0 and decrement to current top value
-    p.ADDI('t0', RSP, 0)
-    p.ADDI('t0', 't0', -4)
-    # push t0 onto stack
-    p.SW(DSP, 't0', 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, '0=', 'ZEROEQUALS'):
-    # pop value into t0
-    p.ADDI(DSP, DSP, -4)
-    p.LW('t0', DSP, 0)
-    # check equality between t0 and 0
-    p.ADDI('t1', 'zero', 0)
-    p.BNE('t0', 'zero', 'notzero')
-    p.ADDI('t1', 't1', -1)  # -1 if zero, 0 otherwise
-with p.LABEL('notzero'):
-    # push result of comparison onto stack
-    p.SW(DSP, 't1', 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-with defword(p, '+', 'PLUS'):
-    # pop first value into t0
-    p.ADDI(DSP, DSP, -4)
-    p.LW('t0', DSP, 0)
-    # pop second value into t1
-    p.ADDI(DSP, DSP, -4)
-    p.LW('t1', DSP, 0)
-    # add the two together into t0
-    p.ADD('t0', 't0', 't1')
-    # push resulting sum onto stack
-    p.SW(DSP, 't0', 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
-
-p.LABEL('latest')  # mark the latest builtin word
-with defword(p, 'nand', 'NAND'):
-    # pop first value into t0
-    p.ADDI(DSP, DSP, -4)
-    p.LW('t0', DSP, 0)
-    # pop second value into t1
-    p.ADDI(DSP, DSP, -4)
-    p.LW('t1', DSP, 0)
-    # AND the two together into t0
-    p.AND('t0', 't0', 't1')
-    # NOT the value in t0
-    p.XORI('t0', 't0', -1)
-    # push resulting value onto stack
-    p.SW(DSP, 't0', 0)
-    p.ADDI(DSP, DSP, 4)
-    # next
-    p.JAL('zero', 'next')
+#with defword(p, 'state', 'STATEVAR'):
+#    # push STATE onto stack
+#    p.SW(DSP, STATE, 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, 'tib', 'TIBVAR'):
+#    # push TIB onto stack
+#    p.SW(DSP, TIB, 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, '>in', 'TOINVAR'):
+#    # push TOIN onto stack
+#    p.SW(DSP, TOIN, 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, 'here', 'HEREVAR'):
+#    # push HERE onto stack
+#    p.SW(DSP, HERE, 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, 'latest', 'LATESTVAR'):
+#    # push LATEST onto stack
+#    p.SW(DSP, LATEST, 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, '@', 'FETCH'):
+#    # pop address into t0
+#    p.ADDI(DSP, DSP, -4)
+#    p.LW('t0', DSP, 0)
+#    # load value from address
+#    p.LW('t0', 't0', 0)
+#    # push value onto stack
+#    p.SW(DSP, 't0', 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, '!', 'STORE'):
+#    # pop address into t0
+#    p.ADDI(DSP, DSP, -4)
+#    p.LW('t0', DSP, 0)
+#    # pop value into t1
+#    p.ADDI(DSP, DSP, -4)
+#    p.LW('t1', DSP, 0)
+#    # store value to address
+#    p.SW('t0', 't1', 0)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, 'sp@', 'SPFETCH'):
+#    # copy DSP into t0 and decrement to current top value
+#    p.ADDI('t0', DSP, 0)
+#    p.ADDI('t0', 't0', -4)
+#    # push t0 onto stack
+#    p.SW(DSP, 't0', 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, 'rp@', 'RPFETCH'):
+#    # copy RSP into t0 and decrement to current top value
+#    p.ADDI('t0', RSP, 0)
+#    p.ADDI('t0', 't0', -4)
+#    # push t0 onto stack
+#    p.SW(DSP, 't0', 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, '0=', 'ZEROEQUALS'):
+#    # pop value into t0
+#    p.ADDI(DSP, DSP, -4)
+#    p.LW('t0', DSP, 0)
+#    # check equality between t0 and 0
+#    p.ADDI('t1', 'zero', 0)
+#    p.BNE('t0', 'zero', 'notzero')
+#    p.ADDI('t1', 't1', -1)  # -1 if zero, 0 otherwise
+#with p.LABEL('notzero'):
+#    # push result of comparison onto stack
+#    p.SW(DSP, 't1', 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#with defword(p, '+', 'PLUS'):
+#    # pop first value into t0
+#    p.ADDI(DSP, DSP, -4)
+#    p.LW('t0', DSP, 0)
+#    # pop second value into t1
+#    p.ADDI(DSP, DSP, -4)
+#    p.LW('t1', DSP, 0)
+#    # add the two together into t0
+#    p.ADD('t0', 't0', 't1')
+#    # push resulting sum onto stack
+#    p.SW(DSP, 't0', 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
+#
+#p.LABEL('latest')  # mark the latest builtin word
+#with defword(p, 'nand', 'NAND'):
+#    # pop first value into t0
+#    p.ADDI(DSP, DSP, -4)
+#    p.LW('t0', DSP, 0)
+#    # pop second value into t1
+#    p.ADDI(DSP, DSP, -4)
+#    p.LW('t1', DSP, 0)
+#    # AND the two together into t0
+#    p.AND('t0', 't0', 't1')
+#    # NOT the value in t0
+#    p.XORI('t0', 't0', -1)
+#    # push resulting value onto stack
+#    p.SW(DSP, 't0', 0)
+#    p.ADDI(DSP, DSP, 4)
+#    # next
+#    p.JAL('zero', 'next')
 
 p.LABEL('here')  # mark the location of the next new word
 
