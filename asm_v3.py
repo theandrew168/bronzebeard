513d43c569bed09abb56d447bf30f67e4fb105d5
theandrew168 <steveday168@gmail.com>
2020-11-10 22:18:36 -0600

    More work on asm v3

--- asm_v3.py
+++ asm_v3.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import struct
 
 
 # definitions for the "items" that can be found in an assembly program
@@ -12,12 +13,14 @@ JTypeInstruction = namedtuple('JTypeInstruction', 'name rd imm')
 Label = namedtuple('Label', 'name')
 Align = namedtuple('Align', 'alignment')
 Blob = namedtuple('Blob', 'data')
+Pack = namedtuple('Pack', 'format value')
 Hi = namedtuple('Hi', 'imm')
 Lo = namedtuple('Lo', 'imm')
 Position = namedtuple('Position', 'label')
 PositionFrom = namedtuple('PositionFrom', 'label imm')
 Offset = namedtuple('Offset', 'label')
-OffsetFrom = namedtuple('OffsetFrom', 'label imm')
+#OffsetFrom = namedtuple('OffsetFrom', 'label imm')
+# OffsetBetween? do the args make sense?
 
 # name rd rs1 rs2
 R_TYPE_INSTRUCTIONS = [
@@ -59,6 +62,8 @@ J_TYPE_INSTRUCTIONS = [
 # 5. Resolve registers  (resolve nice names to integers)
 # 6. Assemble!  (convert everything to bytes)
 
+# these two steps occur in the same pass because they both
+# require the same sort of item inspection and counting
 def resolve_aligns_and_labels(program):
     position = 0
     labels = {}
@@ -68,13 +73,17 @@ def resolve_aligns_and_labels(program):
         if isinstance(item, Label):
             labels[item.name] = position
         elif isinstance(item, Align):
-            data = b''
-            while position % item.alignment != 0:
-                data += b'\x00'
-                position += 1
-            output.append(Blob(data))
+            padding = item.alignment - (position % item.alignment)
+            if padding == item.alignment:
+                continue
+
+            position += padding
+            output.append(Blob(b'\x00' * padding))
         elif isinstance(item, Blob):
-            position += len(item)
+            position += len(item.data)
+            output.append(item)
+        elif isinstance(item, Pack):
+            position += struct.calcsize(item.format)
             output.append(item)
         else:
             position += 4
@@ -115,8 +124,39 @@ def resolve_instructions(program):
 
 
 # example program starts here
+FORTH_SIZE = 16 * 1024  # 16K
+USART_BAUD = 115200
+
 ROM_BASE_ADDR = 0x08000000
 RAM_BASE_ADDR = 0x20000000
+CLOCK_FREQ = 8000000
+
+W = 's0'
+IP = 'gp'
+DSP = 'sp'
+RSP = 'tp'
+
+STATE = 's1'
+TIB = 's2'
+TBUF = 's3'
+TLEN = 's4'
+TPOS = 's5'
+HERE = 's6'
+LATEST = 's7'
+
+INTERPRETER_BASE = 0x0000
+TIB_BASE = 0x1c00
+DATA_STACK_BASE = 0x2000
+RETURN_STACK_BASE = 0x3000
+
+INTERPRETER_SIZE = 0x1c00  # 7K
+TIB_SIZE = 0x0400  # 1K
+DATA_STACK_SIZE = 0x1000  # 4K
+RETURN_STACK_SIZE = 0x1000  # 4K
+
+F_IMMEDIATE = 0b10000000
+F_HIDDEN    = 0b01000000
+F_LENGTH    = 0b00111111
 
 prog = []
 
@@ -130,22 +170,94 @@ prog += [
     UTypeInstruction('lui', 't1', Hi(RAM_BASE_ADDR)),
     ITypeInstruction('addi', 't1', 't1', Lo(RAM_BASE_ADDR)),
     # setup copy count (everything up to "here" label)
-    Instruction('addi', 't2', 0, Position('here'))
-]
+    Instruction('addi', 't2', 0, Position('here')),
 
-prog += [
-    Label('copy_loop'),
-    Instruction('beq', 't2', 'zero', 'copy_done'),
-    Instruction('lw', 't3', 't0', 0),
+    Label('token_skip_whitespace'),
+    Instruction('add', 't1', TBUF, TPOS),
+    Instruction('lbu', 't2', 't1', 0),
+    Instruction('bge', 't2', 't0', Offset('token_scan')),
+    Instruction('addi', TPOS, TPOS, 1),
+    Instruction('jal', 'zero', Offset('token_skip_whitespace')),
+
+    Instruction('lui', HERE, Hi(PositionFrom('here', RAM_BASE_ADDR))),
+    Instruction('addi', HERE, HERE, Lo(PositionFrom('here', RAM_BASE_ADDR))),
+
+    Label('interpreter_interpret'),
+    Instruction('jal', 'ra', Offset('token')),
+
+    # dub ref to interpreter hack
+    Label('interpreter_addr'),
+    Pack('<I', PositionFrom('interpreter_interpret', RAM_BASE_ADDR)),
+    Label('interpreter_addr_addr'),
+    Pack('<I', PositionFrom('interpreter_addr', RAM_BASE_ADDR)),
+
+    Label('next'),
+    Instruction('lw', W, IP, 0),
+    Instruction('addi', IP, IP, 4),
+    Instruction('lw', 't0', W, 0),
+    Instruction('jalr', 'zero', 't0', 0),
+
+    # literal output from defword: +
+    Label('word_+'),
+    Pack('<I', 0),  # link
+    Pack('<B', 1),  # flags | len
+    Blob('+'),  # name
+    Align(4),
+    Pack('<I', PositionFrom('code_+', RAM_BASE_ADDR)),  # code field
+    Label('code_+'),
+    Instruction('addi', DSP, DSP, -4),
+    Instruction('lw', 't0', DSP, 0),
+    Instruction('addi', DSP, DSP, -4),
+    Instruction('lw', 't1', DSP, 0),
+    Instruction('add', 't0', 't0', 't1'),
+    Instruction('sw', DSP, 't0', 0),
+    Instruction('addi', DSP, DSP, 4),
+    Instruction('jal', 'zero', Offset('next')),
+
+    # literal output from defword: nand
+    Label('latest'),
+    Label('word_nand'),
+    Pack('<I', PositionFrom('word_+', RAM_BASE_ADDR)),  # link
+    Pack('<B', 4),  # flags | len
+    Blob('nand'),  # name
+    Align(4),
+    Pack('<I', PositionFrom('code_nand', RAM_BASE_ADDR)),  # code field
+    Label('code_nand'),
+    Instruction('addi', DSP, DSP, -4),
+    Instruction('lw', 't0', DSP, 0),
+    Instruction('addi', DSP, DSP, -4),
+    Instruction('lw', 't1', DSP, 0),
+    Instruction('and', 't0', 't0', 't1'),
+    Instruction('xori', 't0', 't0', -1),
+    Instruction('sw', DSP, 't0', 0),
+    Instruction('addi', DSP, DSP, 4),
+    Instruction('jal', 'zero', Offset('next')),
+
+    Label('here'),
+
+#    Align(FORTH_SIZE),
+    Label('prelude_start'),
+    Blob(': dup sp@ @ ; '),
+    Blob(': -1 dup dup nand dup dup nand nand ; '),
+    Label('prelude_end'),
 ]
 
 
 from pprint import pprint
+
+print('pass 0: raw assembly program')
 pprint(prog)
 
+print('pass 1: resolve aligns and determine label positions')
 prog, labels = resolve_aligns_and_labels(prog)
 pprint(prog)
 pprint(labels)
 
+print('pass 2: resolve generic instructions')
 prog = resolve_instructions(prog)
 pprint(prog)
+
+print('pass 3: resolve immediates')
+print('pass 4: resolve relocations')
+print('pass 5: resolve registers')
+print('pass 6: assemble!')
