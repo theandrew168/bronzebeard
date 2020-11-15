98e4b9574d9fb5a26b41baffb93a23f6e6b4fb97
theandrew168 <steveday168@gmail.com>
2020-11-14 18:34:50 -0600

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='bronzebeard',
-    version='0.0.3',
+    version='0.0.4',
     author='Andrew Dailey',
     description='Minimal ecosystem for bare-metal RISC-V development',
     long_description=readme,
