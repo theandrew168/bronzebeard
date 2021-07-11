84e22ea8c4a818f41d1980ae0cca8f5d97e20f9a
theandrew168 <andrew@shallowbrooksoftware.com>
2021-07-11 17:11:44 -0500

    Initial layout of board support directory

---
+++ boards/chips/GD32VF103.asm
@@ -0,0 +1,4 @@
+# constants related to the GigaDevice GD32VF103 chip
+#
+# datasheet: https://www.gigadevice.com/datasheet/gd32vf103xxxx-datasheet/
+# manual: https://gd32mcu.21ic.com/data/documents/shujushouce/GD32VF103_User_Manual_EN_V1.2.pdf
