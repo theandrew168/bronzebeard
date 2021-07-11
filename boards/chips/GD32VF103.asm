235de63e9d0964b684e9094ff7edb2f01d9438d8
theandrew168 <andrew@shallowbrooksoftware.com>
2021-07-11 17:35:47 -0500

    Add ROM and RAM details for the two current chips

--- boards/chips/GD32VF103.asm
+++ boards/chips/GD32VF103.asm
@@ -2,3 +2,11 @@
 #
 # datasheet: https://www.gigadevice.com/datasheet/gd32vf103xxxx-datasheet/
 # manual: https://gd32mcu.21ic.com/data/documents/shujushouce/GD32VF103_User_Manual_EN_V1.2.pdf
+
+# 128KB @ 0x08000000
+ROM_ADDR = 0x08000000
+ROM_SIZE = 128 * 1024
+
+# 32KB @ 0x20000000
+RAM_ADDR = 0x20000000
+RAM_SIZE = 32 * 1024
