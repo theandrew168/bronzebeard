9a64255837b281e0b0c0b4011cbb3cd13ae08986
theandrew168 <andrew@shallowbrooksoftware.com>
2021-07-15 00:27:51 -0500

    Fill in some GD32VF103 definitions

--- bronzebeard/chips/FE310-G002.asm
+++ bronzebeard/chips/FE310-G002.asm
@@ -2,7 +2,3 @@
 #
 # datasheet: https://sifive.cdn.prismic.io/sifive/4999db8a-432f-45e4-bab2-57007eed0a43_fe310-g002-datasheet-v1p2.pdf 
 # manual: https://sifive.cdn.prismic.io/sifive/654b2b4c-a6dd-4aef-afaf-f7ca89f99583_fe310-g002-manual-v1p0.pdf
-
-# 16KB @ 0x80000000
-RAM_ADDR = 0x80000000
-RAM_SIZE = 16 * 1024
